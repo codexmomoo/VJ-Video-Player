3a490a51da1d98a45590c298a2b40e49f0de4e46
codexmomoo <codexmomo@gmail.com>
2026-03-27 10:08:50 +0530

    info.py

--- info.py
+++ info.py
@@ -2,23 +2,23 @@ import re
 from os import environ
 
 # Bot Session Name
-SESSION = environ.get('SESSION', 'TechVJBot')
+SESSION = environ.get('SESSION', 'CodeXOTTBot')
 
 # Your Telegram Account Api Id And Api Hash
-API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
+API_ID = int(environ.get('API_ID', '34552493'))
+API_HASH = environ.get('API_HASH', '1a9292b5bd04af88dae7a6d1b5a1da71')
 
 # Bot Token, This Is Main Bot
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 # Admin Telegram Account Id For Withdraw Notification Or Anything Else
-ADMIN = int(environ.get('ADMIN', ''))
+ADMIN = int(environ.get('ADMIN', '8435221866'))
 
 # Back Up Bot Token For Fetching Message When Floodwait Comes
 BACKUP_BOT_TOKEN = environ.get('BACKUP_BOT_TOKEN', "")
 
 # Log Channel, In This Channel Your All File Stored.
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1003826113170'))
 
 # Mongodb Database For User Link Click Count Etc Data Store.
 MONGODB_URI = environ.get("MONGODB_URI", "")
@@ -26,10 +26,11 @@ MONGODB_URI = environ.get("MONGODB_URI", "")
 # Stream Url Means Your Deploy Server App Url, Here You Media Will Be Stream And Ads Will Be Shown.
 STREAM_URL = environ.get("STREAM_URL", "")
 
-# This Link Used As Permanent Link That If Your Deploy App Deleted Then You Change Stream Url, So This Link Will Redirect To Stream Url.
-LINK_URL = environ.get("LINK_URL", "")
+# This Link Used As Permanent Link That If Your Deploy App Deleted Then You Change Stream Url,
+# So This Link Will Redirect To Stream Url.
+LINK_URL = environ.get("LINK_URL", "https://codexottlandingpage.blogspot.com/p/codex-ott-landing-page_97.html")
 
-# Others, Not Usefull
+# Others
 PORT = environ.get("PORT", "8080")
 MULTI_CLIENT = False
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
